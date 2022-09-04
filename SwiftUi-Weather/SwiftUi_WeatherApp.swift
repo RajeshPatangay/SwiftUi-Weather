4861e142d94ea3d8d7f525f18edde14435706435
Rajesh Patangay <rajesh.patangay@aa.com>
2022-09-04 13:38:39 -0500

    Initial Commit

---
+++ SwiftUi-Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi-Weather
+//
+//  Created by Rajesh Patangay on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
